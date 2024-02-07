9e5ec4878f9394e3a1de78c54faab6019cad38b9
SHAYANUL HAQ SADI <shayanul.haq@nexdecade.com>
2024-02-07 12:34:06 +0600

    Initial Commit

---
+++ QR Code app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QR Code app
+//
+//  Created by SHAYANUL HAQ SADI on 2/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
